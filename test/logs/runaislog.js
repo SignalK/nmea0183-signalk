b374c5e46e52cc34e651ee2327ecfc12182c5bce
Teppo Kurki <teppo.kurki@iki.fi>
2015-02-08 20:26:54 +0200

    Reorganise test code and start using mocha in package.json to get Travis integration

--- test/logs/runaislog.js
+++ test/logs/runaislog.js
@@ -1,9 +1,9 @@
 var fs      = require('fs');
 var path    = require('path');
-var Parser  = require('../').Parser;
+var Parser  = require('../..').Parser;
 var parser  = new Parser();
 
-fs.createReadStream(path.join(__dirname, './logs/ais.nmea'), { encoding: 'utf8' }).pipe(parser);
+fs.createReadStream(path.join(__dirname, './ais.nmea'), { encoding: 'utf8' }).pipe(parser);
 
 parser.on('sentence', function(signalk, lineno) {
   console.log(JSON.stringify(signalk, null, 2));
