665b36d64e025f251d0992d5cd87c6c3c06edb94
Fabian Tollenaar <fabian@starting-point.nl>
2015-03-20 23:49:00 +0100

    changed license in package.json

--- codecs/_bwc.js
+++ codecs/_bwc.js
@@ -1,13 +1,6 @@
-/* 
- * BWC codec
+/*
+ * Copyright 2015 Fabian Tollenaar <fabian@starting-point.nl>
  * 
- * @repository 		https://github.com/signalk/nmea-signalk
- * @author 			Fabian Tollenaar <fabian@starting-point.nl>
- *
- *
- *
- * Copyright 2014, Fabian Tollenaar
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,6 +12,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+*/
+
+
+/* 
+ * BWC codec
+ * 
+ * @repository 		https://github.com/signalk/nmea-signalk
+ * @author 			Fabian Tollenaar <fabian@starting-point.nl>
  *
  */
 
