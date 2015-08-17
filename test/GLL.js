8e979099360732bf77e5c54dd1709d002f45d225
Teppo Kurki <tjk@iki.fi>
2015-08-17 21:24:23 +0300

    Fix missing label in GLL conversion by outputting delta directly from codec

---
+++ test/GLL.js
@@ -0,0 +1,22 @@
+var chai = require("chai");
+chai.Should();
+chai.use(require('chai-things'));
+
+chai.use(require('signalk-schema').chaiModule);
+
+
+var nmeaLine = "$GPGLL,5958.613,N,02325.928,E,121022,A,D*40";
+
+describe('GLL', function() {
+  it('converts ok', function(done) {
+    parser = new(require('../lib/').Parser)();
+    parser.on('delta', function(delta) {
+      delta.updates[0].values[0].path.should.equal('navigation.position');
+      delta.updates[0].values[0].value.latitude.should.be.closeTo(59.9768833, 0.000005);
+      delta.updates[0].values[0].value.longitude.should.be.closeTo(23.432133, 0.000005);
+      delta.should.be.validSignalKDelta;
+      done();
+    });
+    parser.write(nmeaLine);
+  })
+});
