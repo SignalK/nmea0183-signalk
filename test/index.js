1feb8d2a291428b73cce90e1c19152f816492dee
Fabian Tollenaar <fabian@starting-point.nl>
2015-10-01 21:15:27 +0200

    added a stub test for travis

---
+++ test/index.js
@@ -0,0 +1,12 @@
+var chai = require("chai");
+chai.Should();
+chai.use(require('chai-things'));
+
+
+describe('Replace this stub with real tests once the lite version is finished', function() {
+  it('...or you shall be shot.', function(done) {
+    var t = true;
+    t.should.equal.true; 
+    done();
+  })
+});
