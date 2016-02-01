89c448d100361c52cea8dd398170ade3996ff052
joabakk <github@heiamoss.com>
2016-02-01 22:25:13 +0100

    Add support for RPM and ROT sentences

---
+++ test/RPM.js
@@ -0,0 +1,18 @@
+var chai = require("chai");
+chai.Should();
+chai.use(require('chai-things'));
+
+
+var nmeaLine = "$IIRPM,E,1,2418.2,10.5,A*5F";
+
+describe('RPM', function() {
+  it('converts ok', function(done) {
+    parser = new(require('../lib/').Parser)();
+    parser.on('delta', function(delta) {
+      delta.should.be.validSignalKDelta;
+      delta.updates[0].values[0].path.should.equal('propulsion.rpm');
+      done();
+    });
+    parser.write(nmeaLine);
+  })
+});
