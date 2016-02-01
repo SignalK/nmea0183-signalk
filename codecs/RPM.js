41e0ce56ada5cee9b317d3a1412826aa585c5bb1
joabakk <github@heiamoss.com>
2016-02-01 22:25:13 +0100

    new file:   ../codecs/RPM.js 	new file:   RPM.js

---
+++ codecs/RPM.js
@@ -0,0 +1,56 @@
+/*
+* RPM codec
+*
+* @repository https://github.com/signalk/nmea-signalk
+* @author Bob Moriarty <bobmor99@gmail.com>
+*
+*
+* Copyright 2015, Bob Moriarty
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+"use strict";
+
+/*
+#        1 2 3   4   5 6
+#        | | |   |   | |
+# $--RPM,a,x,x.x,x.x,A*hh<CR><LF>
+# Field Number:
+#  1) Source, S = Shaft, E = Engine
+#  2) Engine or shaft number
+#  3) Speed, Revolutions per minute
+#  4) Propeller pitch, % of maximum, "-" means astern
+#  5) Status, A means data is valid
+#  6) Checksum
+*/
+
+var Codec = require('../lib/NMEA0183');
+
+module.exports = new Codec('RPM', function(multiplexer, input) {
+  var values = input.values;
+  var self = this;
+  var vals = [
+    { path: 'revolutions', value: self.int(values[3]) }
+  ];
+
+  multiplexer
+    .self()
+    .group('propulsion')
+    .source(this.source(input.instrument))
+    .timestamp(this.timestamp())
+    .values(vals)
+  ;
+  return true;
+});
---
+++ test/RPM.js
@@ -0,0 +1,17 @@
+var chai = require("chai");
+chai.Should();
+chai.use(require('chai-things'));
+
+
+var nmeaLine = "$IIRPM,E,1,2418.2,10.5,A";
+
+describe('RPM', function() {
+  it('converts ok', function(done) {
+    parser = new(require('../lib/').Parser)();
+    parser.on('delta', function(delta) {
+      delta.updates[0].values.should.contain.an.item.with.property('path', 'propulsion.revolutions');
+      done();
+    });
+    parser.write(nmeaLine);
+  })
+});
