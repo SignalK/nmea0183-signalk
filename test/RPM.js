e6c14aed00d0037d514982078f6e3c607ea4089f
joabakk <github@heiamoss.com>
2016-02-01 23:08:18 +0100

    Update RPM.js

--- test/RPM.js
+++ test/RPM.js
@@ -3,7 +3,7 @@ chai.Should();
 chai.use(require('chai-things'));
 
 
-var nmeaLine = "$IIRPM,E,1,2418.2,10.5,A";
+var nmeaLine = "$IIRPM,E,1,2418.2,10.5,A*7B";
 
 describe('RPM', function() {
   it('converts ok', function(done) {
