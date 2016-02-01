313eb7edd22e612157223f3ef6d7202028fea321
joabakk <github@heiamoss.com>
2016-02-01 23:20:44 +0100

    modified:   codecs/RPM.js

--- codecs/RPM.js
+++ codecs/RPM.js
@@ -1,14 +1,24 @@
-/* * RPM codec * * @repository https://github.com/signalk/nmea-signalk 
-* @author Joachim Bakke, joabakk * * * Copyright 2016, Joachim Bakke * 
-* Licensed under the Apache License, Version 2.0 (the "License"); * you 
-may not use this file except in compliance with the License. * You may 
-obtain a copy of the License at * * 
-http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by 
-applicable law or agreed to in writing, software * distributed under 
-the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR 
-CONDITIONS OF ANY KIND, either express or implied. * See the License 
-for the specific language governing permissions and * limitations under 
-the License. * 
+/* 
+* RPM codec 
+* 
+* @repository https://github.com/signalk/nmea-signalk 
+* @author Joachim Bakke, joabakk 
+* 
+* 
+* Copyright 2016, Joachim Bakke 
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); 
+* you may not use this file except in compliance with the License. 
+* You may obtain a copy of the License at 
+* 
+* http://www.apache.org/licenses/LICENSE-2.0 
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License. 
+* 
 */ 
 
 "use strict";
@@ -33,7 +43,7 @@ module.exports = new Codec('RPM', function(multiplexer, input) {
       "source": this.source(input.instrument),
       "timestamp": this.timestamp(),
       "values": [{
-        "path": "propulsion.revolutions",
+        "path": "propulsion.propulsion.revolutions",
         "value": this.float(values[3])
       }]
     }],
