afa897919c80e3e06d14427b496ba20e92066677
Teppo Kurki <teppo.kurki@iki.fi>
2016-02-20 09:22:00 +0200

    Reformat

--- codecs/RPM.js
+++ codecs/RPM.js
@@ -1,25 +1,25 @@
 /*
-* RPM codec
-*
-* @repository https://github.com/signalk/nmea-signalk
-* @author Joachim Bakke, joabakk
-*
-*
-* Copyright 2016, Joachim Bakke
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*/
+ * RPM codec
+ *
+ * @repository https://github.com/signalk/nmea-signalk
+ * @author Joachim Bakke, joabakk
+ *
+ *
+ * Copyright 2016, Joachim Bakke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 "use strict";
 
@@ -36,7 +36,7 @@ var Codec = require('../lib/NMEA0183');
 module.exports = new Codec('RPM', function(multiplexer, input) {
   var values = input.values;
 
-	multiplexer.self();
+  multiplexer.self();
 
   multiplexer.add({
     "updates": [{
@@ -52,5 +52,5 @@ module.exports = new Codec('RPM', function(multiplexer, input) {
     }],
     "context": multiplexer._context
   });
-	return true;
+  return true;
 });
