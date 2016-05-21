892a9219b43bf908deac19243878b8ce0097cbd9
Teppo Kurki <teppo.kurki@iki.fi>
2016-05-21 20:25:43 +0300

    Multiline ais (#59)
    Rewrite VDM decoding to handle fields present in different
    VDM sentences individually: generate the delta values
    for which there is data. Add test for multiline & single
    line VDMs.

--- codecs/VDM.js
+++ codecs/VDM.js
@@ -1,10 +1,10 @@
 /*
  * Copyright 2015 Fabian Tollenaar <fabian@starting-point.nl>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -12,51 +12,87 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 
 "use strict";
 
-var Codec   = require('../lib/NMEA0183');
-var Decoder = require ("ggencoder").AisDecode;
+var Codec = require('../lib/NMEA0183');
+var Decoder = require("ggencoder").AisDecode;
+
+/*
+A QUICK HACK THAT WILL NOT WORK WITH MULTIPLE
+AIS NMEA STREAMS. Implements ggencoder session
+as a global singleton. Proper implementation would be
+a per stream constructer NMEA0183 encoder that would be able
+to keep a coherent internal parsing state.
+*/
+var session = {};
 
 module.exports = new Codec('VDM', function(multiplexer, input, line) {
-  var data = new Decoder(line);
+  var data = new Decoder(line, session);
 
-  if(!data.valid) {
-    return this.reportError(this.errors.VOID, "Not parsing sentence for it isn't valid."); 
+  if (!data.valid) {
+    return this.reportError(this.errors.VOID, "Not parsing sentence for it isn't valid.");
   }
 
-  // Set MRN with MMSI
-  data.mmsi = 'urn:mrn:imo:mmsi:' + data.mmsi;
-  // MMSI
-  multiplexer.vessel(data.mmsi).set('mmsi', data.mmsi);
 
-  // Position
-  multiplexer
-    .vessel(data.mmsi)
-    .group('navigation')
-    .set('position', {
-      source: this.source(input.instrument),
-      timestamp: this.timestamp(),
-      longitude: data.lon,
-      latitude: data.lat
+  var values = [];
+  if (data.mmsi) {
+    values.push({
+      path: "",
+      value: {
+        mmsi: data.mmsi
+      }
     })
-  ;
+  }
+  if (data.shipname) {
+    values.push({
+      path: "",
+      value: {
+        name: data.shipname
+      }
+    })
+  }
+  if (typeof data.sog != 'undefined') {
+    values.push({
+      path: "navigation.speedOverGround",
+      value: this.transform(data.sog, 'knots', 'ms')
+    })
+  }
+  if (typeof data.cog != 'undefined') {
+    values.push({
+      path: "navigation.courseOverGroundTrue",
+      value: this.transform(data.cog, 'deg', 'rad')
+    })
+  }
+  if (typeof data.hdg != 'undefined') {
+    values.push({
+      path: "navigation.headingTrue",
+      value: this.transform(data.hdg, 'deg', 'rad')
+    })
+  }
+  if (data.lon && data.lat) {
+    values.push({
+      path: "navigation.position",
+      value: {
+        longitude: data.lon,
+        latitude: data.lat
+      }
+    })
+  }
 
-  // Other values
-  multiplexer
-    .vessel(data.mmsi)
-    .group('navigation')
-    .timestamp(this.timestamp())
-    .source(this.source(input.instrument))
-    .values([
-      { path: "speedOverGround", value: this.transform(data.sog, 'knots', 'ms') },
-      { path: "courseOverGround", value: this.transform(data.cog, 'deg', 'rad') },
-      { path: "state", value: data.GetNavStatus() },
-      { path: "headingTrue", value: this.transform(data.hdg, 'deg', 'rad') }
-    ])
-  ;
+  if (values.length > 0) {
+    multiplexer.self();
+    multiplexer.add({
+      "updates": [{
+        "source": this.source(input.instrument),
+        "timestamp": this.timestamp(),
+        "values": values
+      }],
+      "context": 'vessels.urn:mrn:imo:mmsi:' + data.mmsi
+    });
+  }
 
   return true;
 });
