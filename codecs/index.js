0285470719bc09b231a726e6bc92b3f2ca0a6469
Philip Freeman <elektron@halo.nu>
2016-12-07 08:39:58 -0800

    Add MTW - Mean Temperature of Water (#64)

--- codecs/index.js
+++ codecs/index.js
@@ -32,6 +32,7 @@ var codecs = {
   HDM: require('./HDM'),
   HDG: require('./HDG'),
   HDT: require('./HDT'),
+  MTW: require('./MTW'),
   MWV: require('./MWV'),
   RMC: require('./RMC'),
   VDM: require('./VDM'),
