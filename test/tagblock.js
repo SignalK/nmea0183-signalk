95e643d831bccbb36014e4c63f0826944098fd7b
Fabian Tollenaar <fabian@decipher.industries>
2016-12-15 11:33:42 +0100

    Fixed stream behaviour

--- test/tagblock.js
+++ test/tagblock.js
@@ -3,17 +3,19 @@
 const Parser = require('../lib')
 const chai = require("chai")
 const signalkSchema = require('signalk-schema')
-const nmeaLine = '$IIDBT,035.53,f,010.83,M,005.85,F*23'
+const nmeaLine = '\\s:compass,c:1438489697*13\\$IIDBT,035.53,f,010.83,M,005.85,F*23'
 
 chai.Should()
 chai.use(require('chai-things'))
 
-describe('DBT', () => {
+describe('NMEA0183v4 tag block', () => {
 
   it('Converts OK using individual parser', done => {
     const parser = new Parser
 
     parser.on('signalk:delta', delta => {
+      delta.updates[0].source.should.equal('compass')
+      delta.updates[0].timestamp.should.equal('2015-08-02T04:28:17.000Z')
       delta.updates[0].values.should.contain.an.item.with.property('path', 'environment.depth.belowTransducer')
       done()
     })
@@ -28,6 +30,8 @@ describe('DBT', () => {
     stream.on('data', result => {
       result.should.be.an.object
       result.should.have.property('delta')
+      result.delta.updates[0].source.should.equal('compass')
+      result.delta.updates[0].timestamp.should.equal('2015-08-02T04:28:17.000Z')
       result.delta.updates[0].values.should.contain.an.item.with.property('path', 'environment.depth.belowTransducer')
       done()
     })
