f38c2541666823c66c4de70d603e0715eefb8e00
Johan Hedlund <34450158+johedlund@users.noreply.github.com>
2018-02-12 22:20:17 +0100

    added some PNKEP sentences
    Added support for PNKEP 01, 02 and 03

--- hooks/KEP.js
+++ hooks/KEP.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /**
- * Copyright 2016 Signal K <info@signalk.org> and contributors.
+ * Copyright 2018 Signal K <info@signalk.org> and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
