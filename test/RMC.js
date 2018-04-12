531f8e963c403918ce17655e0f01115ca36ce108
e-sailing <e-sailing@users.noreply.github.com>
2018-04-12 04:42:22 +0200

    seconds are missing (#117)

--- test/RMC.js
+++ test/RMC.js
@@ -28,7 +28,7 @@ describe('RMC', () => {
     const parser = new Parser
 
     parser.on('signalk:delta', delta => {
-      delta.updates[0].timestamp.should.equal('2014-04-03T08:54:00.000Z')
+      delta.updates[0].timestamp.should.equal('2014-04-03T08:54:12.000Z')
       delta.updates[0].values.should.contain.an.item.with.property('path', 'navigation.position')
       delta.updates[0].values.should.contain.an.item.with.property('path', 'navigation.courseOverGroundTrue')
       delta.updates[0].values.should.contain.an.item.with.property('path', 'navigation.speedOverGround')
@@ -37,11 +37,11 @@ describe('RMC', () => {
       delta.updates[0].values.should.contain.an.item.with.property('path', 'navigation.datetime')
       delta.updates[0].values[0].value.latitude.should.be.closeTo(52.372, 0.005)
       delta.updates[0].values[0].value.longitude.should.be.closeTo(4.91, 0.005)
-      delta.updates[0].values[5].value.should.equal('2014-04-03T08:54:00.000Z')
+      delta.updates[0].values[5].value.should.equal('2014-04-03T08:54:12.000Z')
       delta.updates[0].values[1].value.should.be.closeTo(4.387, 0.005)
       delta.updates[0].values[2].value.should.be.closeTo(0.298, 0.005)
       delta.updates[0].values[3].value.should.equal(0)
-      delta.updates[0].values[4].value.should.equal(1396515240)
+      delta.updates[0].values[4].value.should.equal(1396515252)
       done()
     })
 
