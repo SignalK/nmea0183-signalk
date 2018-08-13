373a0287d28d8028ac17365f863a3030a832f7e1
Thomas Sarlandie <thomas@sarlandie.net>
2018-08-13 20:47:54 +0200

    test: add a test case for APB forced-error
    APB always return an error. Add a test so we can confirm that
    error-behaviour does not change while doing some refactoring later.

---
+++ test/APB.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2016 Signal K and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict'
+
+const Parser = require('../lib')
+const chai = require('chai')
+const should = require('chai').Should()
+
+chai.Should()
+chai.use(require('chai-things'))
+
+describe('APB', done => {
+  it('Doesn\'t parse APB sentences', done => {
+    new Parser()
+    .parse('$GPAPB,A,A,0.10,R,N,V,V,011,M,DEST,011,M,011,M*3C')
+    .then(result => {
+      should.equal(result, null)
+      done()
+    })
+    .catch(e => {
+      should.equal(e.message, "@FIXME: APB hook needs to be rewritten to fit latest version of SK")
+      done()
+    })
+  })
+
+})
