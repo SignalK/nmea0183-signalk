dfda30deb630830ddcafe6eea7710633b0cfff0c
Raffaele Montella <raffaele.montella@uniparthenope.it>
2018-08-22 10:18:30 +0000

    Bugfix: changed the key composition in order to make it working with STALK

--- hooks/ALK.js
+++ hooks/ALK.js
@@ -32,9 +32,9 @@ const seatalkHooks = require('./seatalk')
 
 module.exports = function(input) {
   const { id, sentence, parts, tags } = input
-  const key = '0x' + parseInt(parts[0],16).toString(16).toUpperCase()
-  if (typeof seatalkHooks[key] === 'function'){
-    return seatalkHooks[key](input)
+  const key = '0x'+parts[0].toUpperCase()
+  if (key in subHooks){
+    return require(`${path}/${key}`)(parser, input)
   } else {
     return null
   }
