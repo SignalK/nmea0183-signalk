238d57e1d04300df071d54a3f46661407c29ce31
Raffaele Montella <raffaele.montella@uniparthenope.it>
2018-08-22 12:48:42 +0000

    Sub hook for parsing ST datagram related to Magnetic Course

---
+++ hooks/seatalk/0x53.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2016 Signal K <info@signalk.org> and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict'
+
+ const utils = require('@signalk/nmea0183-utilities')
+
+/*
+53  U0  VW      Magnetic Course in degrees: 
+                  The two lower  bits of  U * 90 + 
+                  the six lower  bits of VW *  2 + 
+                  the two higher bits of  U /  2 = 
+                  (U & 0x3) * 90 + (VW & 0x3F) * 2 + (U & 0xC) / 8
+                  The Magnetic Course may be offset by the Compass Variation (see datagram 99) to get the Course Over Ground (COG).
+*/
+
+module.exports = function (parser, input) {
+  const { id, sentence, parts, tags } = input
+
+  var U = (parseInt(parts[1],16) & 0xF0) >> 4;
+  var VW = parseInt(parts[2],16);
+  var magneticCourse=(U & 0x3) * 90.0 + (VW & 0x3F) * 2.0 + (U & 0xC) / 8.0;
+  console.log("magneticCourse:"+magneticCourse)
+  
+  var pathValues = []
+  
+  pathValues.push({
+    path: 'navigation.courseOverGroundMagnetic',
+    value: utils.transform(utils.float(magneticCourse), 'deg', 'rad')
+  })
+                 
+  try {
+
+    const delta = {
+      updates: [
+        {
+          source: tags.source,
+          timestamp: tags.timestamp,
+          values: pathValues
+        }
+      ],
+    }
+
+
+    return Promise.resolve({ delta })
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
