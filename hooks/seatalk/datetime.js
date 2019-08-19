42400bfaac3c8d786a22e84d69a3bcfff5bac2fe
Raffaele Montella <raffaele.montella@uniparthenope.it>
2019-08-19 19:17:14 +0200

    Removed: no more needed because the session based approach.

--- hooks/seatalk/datetime.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var date=null;
-var time=null;
-module.exports.date=date;
-module.exports.time=time;
--- hooks/seatalk/position.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var longitude=null;
-var latitude=null;
-
-module.exports.longitude=longitude;
-module.exports.latitude=latitude;
