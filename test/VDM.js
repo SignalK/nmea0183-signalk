709683c5ca1bf1f04ac6941c674b1388a275ea2e
Scott Bender <scott@scottbender.net>
2020-02-26 12:45:59 -0500

    feature: add support for imo to VDM

--- test/VDM.js
+++ test/VDM.js
@@ -97,5 +97,16 @@ describe('VDM', function() {
     const delta = new Parser().parse('!AIVDM,1,1,,B,13aGra0P00PHid>NK9<2FOvHR624,0*3E\n')
     delta.updates[0].values.find(pathValue => pathValue.path === 'navigation.state').value.should.equal('motoring')
     delta.updates[0].values.find(pathValue => pathValue.path === 'sensors.ais.class').value.should.equal('A')
-   })
+  })
+
+  it('imo conerts ok', () => {
+    const parser = new Parser()
+    let delta = parser.parse('!AIVDM,2,1,9,A,54hi<240?JU9`L<f220l4T@DhhF222222222220U5HD2:40Ht90000000000,0*60')
+    should.equal(delta, null)
+
+    delta = parser.parse('!AIVDM,2,2,9,A,00000000002,2*2F')
+
+    delta.context.should.equal('vessels.urn:mrn:imo:mmsi:319573000')
+    delta.updates[0].values.filter(pathValue => pathValue.path === '')[3].value.registrations.imo.should.equal('IMO 1010258')
+  })
 })
