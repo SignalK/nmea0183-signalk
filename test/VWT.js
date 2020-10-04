a369a4c4cbf10be258957e6ae7c2d6ab5f0e70d0
Hans-Peter Bergander <peter@bergander.net>
2020-10-04 09:34:53 +0200

    cosmetics: removed whitespace

--- test/VWT.js
+++ test/VWT.js
@@ -11,7 +11,7 @@ describe('VWT', () => {
     const delta = new Parser().parse('$IIVWT,030.,R,10.1,N,05.2,M,018.7,K*75')
 //'$IIMWV,074,T,05.85,N,A*2E'
 // $IIVWT,030.,R,10.1,N,05.2,M,018.7,K*75
-      
+
     delta.updates[0].values.should.contain.an.item.with.property('path', 'environment.wind.angleTrueWater')
       delta.updates[0].values[1].value.should.be.closeTo(0.523599, 0.005)
   })
