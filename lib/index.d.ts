f8a281e18c99aebfb1e7e9f2fa19edec069c1f68
David Revay <daverevay@gmail.com>
2021-09-06 21:33:05 +1000

    feat: add Parser ts types

---
+++ lib/index.d.ts
@@ -0,0 +1,64 @@
+export interface IParserDelta {
+  updates: {
+    source: string;
+    timestamp: string;
+    values: {
+      path: string;
+      value: any;
+    }[];
+  }[];
+}
+
+export interface IParserDeltaWithSource {
+  updates: {
+    source: {
+      sentence: string;
+      talker: string;
+      type: string;
+    };
+    timestamp: string;
+    values: {
+      path: string;
+      value: any;
+    }[];
+  }[];
+}
+
+export interface IParserArgs {
+  id: string;
+  sentence: string;
+  parts: string[];
+  tags: {
+    source: string;
+    timestamp: string;
+  };
+}
+export interface IParserSession {
+  [key: string]: any;
+}
+
+export interface IPropertyValue {
+  value: {
+    sentence: string;
+    parser: (args: IParserArgs, session: IParserSession) => null | IParserDelta;
+  };
+}
+
+export type IParserOptionsOnPropertyValuesCallback = (
+  val: undefined | IPropertyValue[],
+) => void;
+
+export interface IParserOptions {
+  onPropertyValues?: (
+    propertyName: string,
+    onPropertyValuesCallback: IParserOptionsOnPropertyValuesCallback,
+  ) => any;
+  validateChecksum?: boolean;
+}
+
+declare class Parser {
+  constructor(opts?: IParserOptions);
+  parse<T = (IParserDeltaWithSource | null)>(sentence: string): T;
+}
+
+export default Parser;
