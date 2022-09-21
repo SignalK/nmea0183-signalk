b32d715d3be12d0b1c06241f8df5501863ded025
Fabian Tollenaar <fabian@decipher.industries>
2022-09-21 12:09:20 +0200

    Add dictionary resolving

--- hooks/XDR.js
+++ hooks/XDR.js
@@ -1,8 +1,22 @@
 // $IIXDR,C,C,10.7,C,AIRTEMP,A,0.5,D,HEEL,A,-1.-3,D,TRIM,P,1.026,B,BARO,A,A,-4.-3,D,RUDDER*18
 
-const xdrDictionary = require('xdr-parser-plugin/xdrDict');
+const fs = require('fs');
 const xdrParser = require('xdr-parser-plugin/xdrParser');
+const xdrDictionary = { definitions: [] };
 
+// Populate a dictionary
+const xdrDictPath = require.resolve('xdr-parser-plugin/xdrDict');
+if (fs.existsSync(xdrDictPath)) {
+  try {
+    const json = JSON.parse(fs.readFileSync(xdrDictPath, 'utf-8'));
+
+    if (json && Array.isArray(json.definitions)) {
+      xdrDictionary.definitions = [ ...json.definitions ];
+    }
+  } catch (err) {
+    console.warn('No dictionary found for xdr-parser-plugin');
+  }
+}
 
 module.exports = function (input) {
   const { sentence } = input
