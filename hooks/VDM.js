629db228d18859065bf0056cc2c24e2c63a6aa94
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2024-02-17 08:20:51 +0200

    Meteorological and hydrographic data from AIS VDM binary message 8 (#245)

--- hooks/VDM.js
+++ hooks/VDM.js
@@ -20,6 +20,11 @@ const debug = require('debug')('signalk-parser-nmea0183/VDM')
 const utils = require('@signalk/nmea0183-utilities')
 const Decoder = require('ggencoder').AisDecode
 const schema = require('@signalk/signalk-schema')
+const knotsToMs = (v) =>
+  parseFloat(utils.transform(v, 'knots', 'ms').toFixed(2))
+const degToRad = (v) => utils.transform(v, 'deg', 'rad')
+const cToK = (v) => parseFloat(utils.transform(v, 'c', 'k').toFixed(2))
+const nmToM = (v) => parseFloat(utils.transform(v, 'nm', 'm').toFixed(2))
 
 const stateMapping = {
   0: 'motoring',
@@ -67,6 +72,50 @@ const specialManeuverMapping = {
   3: 'reserved',
 }
 
+const beaufortScale = {
+  0: 'calm, 0-0.2 m/s',
+  1: 'light air, 0.3-1.5 m/s',
+  2: 'light breeze, 1.6-3.3 m/s',
+  3: 'gentle breeze, 3.4-5.4 m/s',
+  4: 'moderate breeze, 5.5-7.9 m/s',
+  5: 'fresh breeze, 8-10.7 m/s',
+  6: 'strong breeze, 10.8-13.8 m/s',
+  7: 'high wind, 13.9-17.1 m/s',
+  8: 'gale, 17.2-20.7 m/s',
+  9: 'strong gale, 20.8-24.4 m/s',
+  10: 'storm, 24.5-28.4 m/s',
+  11: 'violent storm, 28.5-32.6 m/s',
+  12: 'hurricane-force, ≥ 32.7 m/s',
+  13: 'not available',
+  14: 'reserved',
+  15: 'reserved',
+}
+
+const statusTable = {
+  0: 'steady',
+  1: 'decreasing',
+  2: 'increasing',
+  3: 'not available',
+}
+
+const precipitationType = {
+  0: 'reserved',
+  1: 'rain',
+  2: 'thunderstorm',
+  3: 'freezing rain',
+  4: 'mixed/ice',
+  5: 'snow',
+  6: 'reserved',
+  7: 'not available',
+}
+
+const iceTable = {
+  0: 'no',
+  1: 'yes',
+  2: 'reserved',
+  3: 'not available',
+}
+
 module.exports = function (input, session) {
   const { id, sentence, parts, tags } = input
   const data = new Decoder(sentence, session)
@@ -112,16 +161,6 @@ module.exports = function (input, session) {
     values.push({
       path: 'navigation.headingTrue',
       value: utils.transform(data.hdg, 'deg', 'rad'),
-    })
-  }
-
-  if (data.lon && data.lat) {
-    values.push({
-      path: 'navigation.position',
-      value: {
-        longitude: data.lon,
-        latitude: data.lat,
-      },
     })
   }
 
@@ -262,9 +301,109 @@ module.exports = function (input, session) {
   }
 
   if (typeof data.fid !== 'undefined') {
+    if (data.fid == 31 || data.fid == 11 || data.fid == 33) {
+      contextPrefix = 'meteo.'
+    }
     values.push({
       path: 'sensors.ais.functionalId',
       value: data.fid,
+    })
+  }
+
+  if (data.lon && data.lat) {
+    values.push({
+      path: 'navigation.position',
+      value: {
+        longitude: data.lon,
+        latitude: data.lat,
+      },
+    })
+  }
+
+  ;[
+    ['avgwindspd', 'wind.averageSpeed', knotsToMs],
+    ['windgust', 'wind.gust', knotsToMs],
+    ['winddir', 'wind.directionTrue', degToRad],
+    ['windgustdir', 'wind.gustDirectionTrue', degToRad],
+    ['airtemp', 'outside.temperature', cToK],
+    ['relhumid', 'outside.relativeHumidity', (v) => v],
+    ['dewpoint', 'outside.dewPointTemperature', cToK],
+    ['airpress', 'outside.pressure', (v) => v * 100],
+    ['waterlevel', 'water.level', (v) => v],
+    ['signwavewhgt', 'water.waves.significantHeight', (v) => v],
+    ['waveperiod', 'water.waves.period', (v) => v],
+    ['wavedir', 'water.waves.directionTrue', degToRad],
+    ['swellhgt', 'water.swell.height', (v) => v],
+    ['swellperiod', 'water.swell.period', (v) => v],
+    ['swelldir', 'water.swell.directionTrue', degToRad],
+    ['watertemp', 'water.temperature', cToK],
+    ['salinity', 'water.salinity', (v) => v],
+    ['surfcurrspd', 'water.current.drift', knotsToMs],
+    ['surfcurrdir', 'water.current.set', degToRad],
+  ].forEach(([propName, path, f]) => {
+    if (data[propName] !== undefined) {
+      contextPrefix = 'meteo.'
+      values.push({
+        path: `environment.` + path,
+        value: f(data[propName]),
+      })
+    }
+  })
+    ;[
+      ['ice', 'water.ice', iceTable],
+      ['precipitation', 'outside.precipitation', precipitationType],
+      ['seastate', 'water.seaState', beaufortScale],
+      ['waterlevelten', 'water.levelTendency', statusTable],
+      ['airpressten', 'outside.pressureTendency', statusTable],
+    ].forEach(([propName, path, f]) => {
+      if (data[propName] !== undefined) {
+        contextPrefix = 'meteo.'
+        values.push(
+          {
+            path: `environment.` + path,
+            value: f[data[propName]],
+          },
+          {
+            path: `environment.` + path + `Value`,
+            value: data[propName],
+          }
+        )
+      }
+    })
+
+  if (data.horvisib !== undefined && data.horvisibrange !== undefined) {
+    contextPrefix = 'meteo.'
+    values.push(
+      {
+        path: 'environment.outside.horizontalVisibility',
+        value: utils.transform(data.horvisib, 'nm', 'm'),
+      },
+      {
+        path: 'environment.outside.horizontalVisibility.overRange',
+        value: data.horvisibrange,
+      }
+    )
+  }
+
+  if (
+    data.utcday !== undefined &&
+    data.utchour !== undefined &&
+    data.utcminute !== undefined
+  ) {
+    contextPrefix = 'meteo.'
+    const y = new Date().getUTCFullYear()
+    const m = new Date().getUTCMonth() + 1
+    const d = data.utcday
+    const h = data.utchour
+    const min = data.utcminute
+    const date = `${y}-${m.toString().padStart(2, '0')}-${d
+      .toString()
+      .padStart(2, '0')}T${h.toString().padStart(2, '0')}:${min
+      .toString()
+      .padStart(2, '0')}:00.000Z`
+    values.push({
+      path: 'environment.date',
+      value: date,
     })
   }
 
@@ -273,7 +412,7 @@ module.exports = function (input, session) {
   }
 
   const delta = {
-    context: contextPrefix + `urn:mrn:imo:mmsi:${data.mmsi}`,
+    context: contextPrefix + `urn:mrn:imo:mmsi:${data.mmsikey || data.mmsi}`,
     updates: [
       {
         source: tags.source,
