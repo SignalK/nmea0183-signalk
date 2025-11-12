99ae30d18af961eaafdc6cc21ea29ae927a5e1db
Antonin Delolme <delolme.antonin07@live.com>
2025-11-12 11:02:03 +0100

    update license

--- test/PASHR.js
+++ test/PASHR.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Signal K and contributors.
+ * Copyright 2025 Signal K and Fabian Tollenaar <fabian@signalk.org>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
